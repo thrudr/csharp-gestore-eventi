423ceba495fae7064d9851e7bff01000385efba6
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-28 13:14:32 +0200

    creazione-della-classe-programma-evento

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,33 +1,36 @@
 ﻿
 using GestoreEventi;
 
+//MILESTONE 2
 Console.WriteLine("Inserisci il nome dell'evento:");
-string nomeEventoUtente = Console.ReadLine();
+    string nomeEventoUtente = Console.ReadLine();
 
 Console.WriteLine("Inserisci la data dell'evento (dd/MM/yyyy):");
-String dataEventoUtente = Console.ReadLine();
+    string dataEventoUtente = Console.ReadLine();
 
 Console.WriteLine("Inserisci il numero di posti totali:");
-int numeroTotalePostiEventoUtente = int.Parse(Console.ReadLine());
+    int numeroTotalePostiEventoUtente = int.Parse(Console.ReadLine());
 
 Evento eventoInserito = new Evento(nomeEventoUtente, dataEventoUtente, numeroTotalePostiEventoUtente);
 
 Console.WriteLine("Inserisci post Prenotati:");
-int numeroPostiPrenotati = int.Parse(Console.ReadLine());
-eventoInserito.PrenotaPosti(numeroPostiPrenotati);
+    int numeroPostiPrenotati = int.Parse(Console.ReadLine());
+        eventoInserito.PrenotaPosti(numeroPostiPrenotati);
 
 Console.WriteLine(eventoInserito.ToString());
 
 Console.WriteLine("Quanti posti desideri prenotare?");
-int quantitaPostiUtente = int.Parse(Console.ReadLine());
-eventoInserito.PrenotaPosti(quantitaPostiUtente);
+    int quantitaPostiUtente = int.Parse(Console.ReadLine());
+        eventoInserito.PrenotaPosti(quantitaPostiUtente);
 
 int postiDisponibili = eventoInserito.GetMaxPosti() - eventoInserito.GetPostiPrenotati();
+
 Console.WriteLine("I posti disponibili sono: " + postiDisponibili);
 Console.WriteLine("I posti prenotati sono: " + eventoInserito.GetPostiPrenotati());
 
 Console.WriteLine("Vuoi disdire dei posti? (si/no)?");
-string check = Console.ReadLine();
+    string check = Console.ReadLine();
+
 while (check == "si")
 {
     Console.WriteLine("Indica il numero di posti da disdire:");
---
+++ GestoreEventi/ProgrammaEventi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class ProgrammaEventi
+    {
+        private string titolo;
+        private List<Evento> eventi;
+    }
+}
