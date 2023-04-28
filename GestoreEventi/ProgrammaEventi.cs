bbd2d62286ee52d23932496f9ca74da73c3e21d4
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-28 15:28:18 +0200

    metodi-lista-eventi: aggiungi-evento; eventi-in-data

--- GestoreEventi/ProgrammaEventi.cs
+++ GestoreEventi/ProgrammaEventi.cs
@@ -18,6 +18,25 @@ namespace GestoreEventi
             this.eventi = new List<Evento>();
         }
 
+        //METODI
+        public void AggiungeEvento(Evento evento)
+        {
+            this.eventi.Add(evento);
+        }
+
+        public List<Evento> eventiInData(string data)
+        {
+            List<Evento> eventiInData = new List<Evento>();
+            DateTime dataConvertita = DateTime.Parse(data);
+            foreach (Evento evento in this.eventi)
+            {
+                if (dataConvertita == evento.GetData())
+                {
+                    eventiInData.Add(evento);
+                }
+            }
+            return eventiInData;
+        }
 
     }
 }
